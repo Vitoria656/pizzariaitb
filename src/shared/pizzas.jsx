17c49c0a1c3b026124f33661468f586422010be5
vitoria656 <vjordana101@gmail.com>
2024-08-09 12:17:07 -0300

    pizzaria

---
+++ src/shared/pizzas.jsx
@@ -0,0 +1,44 @@
+import mussarela from "../assets/imagens/mussarela.png"
+import calabresa from "../assets/imagens/calabresa.png"
+import atum from "../assets/imagens/atum.png"
+import portuguesa from "../assets/imagens/portuguesa.png"
+import frango from "../assets/imagens/frango.png"
+
+const Pizzas = [
+{
+    "id":1,
+    "nome": "Mussarela",
+    "descrição": "Molho de tomate e mussarela.",
+    "imagem": mussarela
+},
+
+{
+    "id":2,
+    "nome": "Frango com Catupiry",
+    "descrição": "Molho de tomate e mussarela, frango desfiado e catupiry.",
+    "imagem": frango
+},
+
+{
+    "id":3,
+    "nome": "Portuguesa",
+    "descrição": "Molho de tomate e mussarela, presunto, ovo, cebola, azeitona e orégano.",
+    "imagem": portuguesa
+},
+
+{
+    "id":4,
+    "nome": "Calabresa",
+    "descrição": "Molho de tomate e mussarela, calabresa e cebola.",
+    "imagem": calabresa
+},
+
+{
+    "id":5,
+    "nome": "Atum",
+    "descrição": "Molho de tomate e mussarela, atum e tomate.",
+    "imagem": atum
+},
+]
+
+export default Pizzas;
